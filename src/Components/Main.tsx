eb098b5bd08b9bad8815fc4a0aec55032c850816
Yoann <maulineyoann@gmail.com>
2025-01-10 15:58:40 +0100

    update request function

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,31 +1,47 @@
-import { useEffect, useState } from "react"
-import Input from "./Input"
-import Countries from "./Countries"
-import FilterComponent from "./FilterComponent"
+import { useEffect, useState } from "react";
+import Input from "./Input";
+import Countries from "./Countries";
+import FilterComponent from "./FilterComponent";
 
 interface Country {
   flag: string;
   name: {
-    common: string; 
-    official?: string; 
+    common: string;
+    official?: string;
   };
-  population: number; 
-  area: number; 
-  region: string; 
+  population: number;
+  area: number;
+  region: string;
 }
 
 const Main = () => {
-  const [data, setData] = useState<Country[]>([])
-  const [search, setSearch] = useState("")
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<Country[]>([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((err) => console.log(err))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   }, []);
 
+  if (loading) return <p>Chargement des données ...</p>
+  if (error) return <p>Error: {error}</p>
+  
 
   return (
     <main className="bg-[#282B30] min-h-screen border border-[#6C727F] relative bottom-20 rounded-xl p-8 text-primary">
@@ -33,15 +49,15 @@ const Main = () => {
         <h4 className="text-primary font-bold tracking-wide">
           Found {data.length} countries
         </h4>
-        <Input state={{search, setSearch}}/>
+        <Input state={{ search, setSearch }} />
       </div>
       <section className="flex gap-14 justify-center mt-10">
         <FilterComponent />
-        <Countries data={data} search={search}/>
+        <Countries data={data} search={search} />
       </section>
     </main>
-  )
-}
+  );
+};
 
-Main.displayName = "MainComponent"
-export default Main
+Main.displayName = "MainComponent";
+export default Main;
